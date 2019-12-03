90446c12e45cacd30f9cebf7f8f4dbc9a6410741
Payton Swick <payton@foolord.com>
2019-12-03 11:11:50 -0500

    Add siteId to createPayPalMethod

--- client/my-sites/checkout/checkout/composite-checkout-container.js
+++ client/my-sites/checkout/checkout/composite-checkout-container.js
@@ -257,6 +257,7 @@ function useCreatePaymentMethods() {
 	const paypalMethod = useMemo(
 		() =>
 			createPayPalMethod( {
+				getSiteId: () => select( 'wpcom' )?.getSiteId?.(),
 				registerStore: registerStore,
 				makePayPalExpressRequest: mockPayPalExpressRequest,
 			} ),
