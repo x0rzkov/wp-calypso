d5f4dda970e79fcbfd334ff76306c74b022eb5aa
stojdza <ivanstojadinov@gmail.com>
2019-12-12 14:24:44 +0100

    Follow first site from the list of the suggested sites to follow

--- test/e2e/lib/pages/reader-page.js
+++ test/e2e/lib/pages/reader-page.js
@@ -31,15 +31,32 @@ export default class ReaderPage extends AsyncBaseContainer {
 		const hasSharablePost = await driverHelper.isElementPresent( this.driver, shareButtonSelector );
 
 		if ( ! hasSharablePost ) {
-			// no shareable posts on this screen. try moving into a combined card
-			const firstComboCardPostSelector = by.css( '.reader-combined-card__post-title-link' );
-			await driverHelper.clickWhenClickable( this.driver, firstComboCardPostSelector );
+			await this.addSiteToFollow();
 		}
 
 		await driverHelper.clickWhenClickable( this.driver, shareButtonSelector );
 		return await driverHelper.clickWhenClickable(
 			this.driver,
 			by.css( '.reader-popover .site__content' )
+		);
+	}
+
+	async addSiteToFollow() {
+		// If user doesn't follow any site add first site from the suggested ones
+		await driverHelper.clickWhenClickable(
+			this.driver,
+			by.css( '.empty-content__action.button.is-primary' )
+		);
+		await driverHelper.clickWhenClickable( this.driver, by.css( '.follow-button__label' ) );
+		await driverHelper.verifyTextPresent(
+			this.driver,
+			by.css( '.follow-button__label' ),
+			'Following'
+		);
+
+		return await driverHelper.clickWhenClickable(
+			this.driver,
+			by.css( '.menu-link-text[data-e2e-sidebar="Followed Sites"]' )
 		);
 	}
 
